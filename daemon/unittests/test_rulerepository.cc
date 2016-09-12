2f25924319a0c92a35dbc4cd0519b275bafc27b7
Danny Edel <mail@danny-edel.de>
2016-09-12 15:18:08 +0200

    add tests for rule repository

---
+++ daemon/unittests/test_rulerepository.cc
@@ -0,0 +1,26 @@
+#include <rulerepository.hh>
+#include <boost/test/unit_test.hpp>
+
+using namespace std;
+using namespace PersonalFirewall;
+using namespace boost::property_tree;
+
+BOOST_AUTO_TEST_SUITE(rulerepository)
+
+BOOST_AUTO_TEST_CASE(emptyreturnsdefault)
+{
+	vector<Verdict> testthis = {
+		Verdict::accept,
+		Verdict::reject,
+		Verdict::undecided,
+	};
+
+	for( const auto& verdict: testthis) {
+		RuleRepository rr( verdict, "/dev/null");
+		Packet p{ ptree() };
+		BOOST_CHECK_EQUAL( verdict, rr.processPacket(p) );
+	}
+
+}
+
+BOOST_AUTO_TEST_SUITE_END();
