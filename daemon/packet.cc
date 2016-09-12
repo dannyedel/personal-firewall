a8c4454120880876b87e818599852489379c02aa
Danny Edel <mail@danny-edel.de>
2016-09-12 15:03:49 +0200

    add exception code

--- daemon/packet.cc
+++ daemon/packet.cc
@@ -62,7 +62,41 @@ bool PersonalFirewall::is_valid_fact_key(const string& s) {
 	return binary_search( validFactsKeys.cbegin(), validFactsKeys.cend(), s);
 }
 
+bool PersonalFirewall::is_valid_metadata_key(const string& s) {
+	return binary_search( validMetadataKeys.cbegin(), validMetadataKeys.cend(), s);
+}
+
 InvalidKey::InvalidKey(const string& keyName, const string& context):
 	runtime_error("The key name "+keyName+" is not valid in the context "+context)
 {
+}
+
+InvalidFactsKey::InvalidFactsKey(const string& keyName):
+	InvalidKey(keyName, "facts")
+{
+}
+
+InvalidMetadataKey::InvalidMetadataKey(const string& keyName):
+	InvalidKey(keyName, "metadata")
+{
+}
+
+void Packet::validate_keys() {
+	validate_facts_keys(facts);
+	validate_metadata_keys(metadata);
+}
+
+void PersonalFirewall::validate_facts_keys(const ptree& facts){
+	for(const auto& pair: facts) {
+		if ( ! is_valid_fact_key(pair.first) ) {
+			throw InvalidFactsKey(pair.first);
+		}
+	}
+}
+
+void PersonalFirewall::validate_metadata_keys(const ptree& metadata){
+	for(const auto& pair: metadata) {
+		if ( ! is_valid_metadata_key(pair.first) )
+			throw InvalidMetadataKey(pair.first);
+	}
 }
