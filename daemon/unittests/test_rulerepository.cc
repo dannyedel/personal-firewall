57b65c0df7283d8abd0d2659fa830d3d2e2c52a7
Danny Edel <mail@danny-edel.de>
2016-09-13 09:38:53 +0200

    add test: rule repository preserves order

--- daemon/unittests/test_rulerepository.cc
+++ daemon/unittests/test_rulerepository.cc
@@ -25,4 +25,29 @@ BOOST_AUTO_TEST_CASE(emptyreturnsdefault)
 
 }
 
+BOOST_AUTO_TEST_CASE(orderpreserve)
+{
+	ptree t1;
+	t1.put("hostname", "b");
+	Rule r1{t1, Verdict::accept};
+
+	ptree t2;
+	t2.put("hostname", "a");
+	Rule r2{t2, Verdict::accept};
+
+	// Assert the rules are not equal
+	BOOST_CHECK_NE(r1, r2);
+
+	// Check that the insertion order matters
+	RuleRepository rr(Verdict::undecided);
+
+	// append copies, instead of moving
+	rr.append_rule(Rule(r1));
+	rr.append_rule(Rule(r2));
+
+	BOOST_CHECK_EQUAL(r1, rr.rules().at(0));
+
+	BOOST_CHECK_EQUAL(r2, rr.rules().at(1));
+}
+
 BOOST_AUTO_TEST_SUITE_END();
